23bfde07f91fdbf3f482c7371f7742bcae02f277
Angelo Zanatta <angeloantoquirinozanatta@gmail.com>
2025-03-24 10:10:25 -0300

    alguns testes em aula

---
+++ curso de python/estudo001.py
@@ -0,0 +1,7 @@
+print(f"Olá Mundo/Hello World!")
+10==10
+print (type(10))
+temperatura_fahrenheit=float(input("Digite uma temperatura em Fahrenheit para converte-la: "))
+temperatura_celsius = (temperatura_fahrenheit-32)*5/9
+print(f"A temperatura em celsius é {temperatura_celsius}")
+print ("olá" 'mundo')
