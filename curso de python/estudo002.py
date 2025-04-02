b643350424b4bb39a6dbf8e3e057b2fd6e326656
Angelo Zanatta <angeloantoquirinozanatta@gmail.com>
2025-04-02 13:46:21 -0300

    Create estudo002.py

---
+++ curso de python/estudo002.py
@@ -0,0 +1,10 @@
+bool = 5 > 3
+print (bool)
+fizer_sol = True 
+for_feriado = False 
+vou_para_pria = fizer_sol and for_feriado
+print (vou_para_pria)
+idoso=False
+estudante=True
+meia_entrada= idoso or estudante 
+print (meia_entrada)
